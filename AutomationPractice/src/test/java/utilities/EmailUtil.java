a4f25c30dbfea788a3ef820b81394e10e10b955f
AbhiYadavSdet <abhi726yadav@gmail.com>
2024-06-24 13:16:32 +0530

    removed space for testing push

--- AutomationPractice/src/test/java/utilities/EmailUtil.java
+++ AutomationPractice/src/test/java/utilities/EmailUtil.java
@@ -5,6 +5,9 @@ import javax.activation.DataSource;
 import javax.activation.FileDataSource;
 import javax.mail.*;
 import javax.mail.internet.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Properties;
 
 public class EmailUtil {
@@ -71,46 +74,58 @@ public class EmailUtil {
         }
     }
 
-        public static void sendEmailWithReport(String to, String subject, String report, String email, String emailPassword) {
-            // Setup mail server
-            Properties props = new Properties();
-            props.put("mail.smtp.host", "smtp.gmail.com");
-            props.put("mail.smtp.port", "587");
-            props.put("mail.smtp.auth", "true");
-            props.put("mail.smtp.starttls.enable", "true");
+    public static void sendEmailWithReport(String to, String subject, String reportPath, String email, String emailPassword) {
+        // Setup mail server
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
 
-            // Get the Session object
-            Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-                protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication(email, emailPassword);
+        // Get the Session object
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(email, emailPassword);
+            }
+        });
+
+        try {
+            // Read the content of the report file
+            StringBuilder reportContent = new StringBuilder();
+            try (BufferedReader br = new BufferedReader(new FileReader(reportPath))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    reportContent.append(line);
+                    reportContent.append("\n");
                 }
-            });
-
-            try {
-                // Create a default MimeMessage object
-                Message message = new MimeMessage(session);
-
-                // Set From: header field of the header
-                message.setFrom(new InternetAddress(email));
-
-                // Set To: header field of the header
-                message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
-
-                // Set Subject: header field
-                message.setSubject(subject);
-
-                // Now set the actual message
-                message.setText(report);
-
-                // Send message
-                Transport.send(message);
-
-                System.out.println("Sent message successfully....");
-
-            } catch (MessagingException e) {
-                throw new RuntimeException(e);
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new RuntimeException("Failed to read the report file.");
             }
 
+            // Create a default MimeMessage object
+            Message message = new MimeMessage(session);
+
+            // Set From: header field of the header
+            message.setFrom(new InternetAddress(email));
+
+            // Set To: header field of the header
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+
+            // Set Subject: header field
+            message.setSubject(subject);
+
+            // Now set the actual message
+            message.setContent(reportContent.toString(), "text/html; charset=utf-8");
+
+            // Send message
+            Transport.send(message);
+
+            System.out.println("Sent message successfully....");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
